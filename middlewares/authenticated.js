c0fb631953e56980c35e4355544283b6106c6679
manugarderes <69734460+manugarderes@users.noreply.github.com>
2022-05-31 12:27:44 -0300

    Login y Register desde WElcome + MiddleWares

---
+++ middlewares/authenticated.js
@@ -0,0 +1,14 @@
+module.exports = {
+    isLoggedIn: (req, res, next) => {
+        if (req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect("/login");
+    },
+    isNotLoggedIn: (req, res, next) => {
+        if (!req.isAuthenticated()) {
+            return next();
+        }
+        res.redirect("/home");
+    }
+}
--- routes/routes.js
+++ routes/routes.js
@@ -1,17 +1,18 @@
 const express = require("express");
 const router = express.Router();
 const controller = require("../controllers/controller");
+const { isLoggedIn, isNotLoggedIn } = require("../middlewares/authenticated");
 
 router.get("/", controller.showWelcome);
-router.get("/home", controller.showHome);
+router.get("/home", isLoggedIn, controller.showHome);
 router.get("/register", controller.showRegister);
-router.get("/login", controller.showLogin);
+router.get("/login", isNotLoggedIn, controller.showLogin);
 router.get("/failedLogin", controller.showFailedLogin);
 
-router.post("/tweet", controller.createTweet);
+router.post("/tweet", isLoggedIn, controller.createTweet);
 router.post("/register", controller.createUser);
-router.post("/login", controller.login);
-router.get("/logout", function (req, res, next) {
+router.post("/login", isNotLoggedIn, controller.login);
+router.get("/logout", isLoggedIn, function (req, res, next) {
   req.logout(function (err) {
     if (err) {
       return next(err);
